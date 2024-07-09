27fbc6336dcd4744dc4bc677d4f3824c2e5ee653
iacoleite <caio.cobucci@gmail.com>
2024-07-09 15:59:29 +0200

    cleaning

---
+++ esercizio3/booleans.py
@@ -0,0 +1,18 @@
+# x = True;
+
+# print(type(x));
+
+# x=5
+
+# y = (x!= 5)
+
+# print(y)
+
+#  Operatore AND
+
+prima_verifica = 4==5
+seconda_verifica = 3>=2
+risultato = prima_verifica and seconda_verifica
+
+print(risultato)
+
